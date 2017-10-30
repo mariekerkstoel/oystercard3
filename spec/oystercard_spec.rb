5ef5e295aac64dabef2481c3bad00dfae8c863e2
Ignacio Panebianco <ignaciopanebianco@gmail.com>
2017-10-30 15:59:13 +0000

    Adds balance defaulted to 0

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,10 @@
 require 'oystercard'
 
 describe Oystercard do
+  let(:card) { described_class.new }
+  describe '#balance' do
+    it 'should be initialized with a default value of 0' do
+      expect(card.balance).to eq(0)
+    end
+  end
 end
