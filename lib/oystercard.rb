c1341394db16b5ca2a51492e083c17985b8dbe87
Ignacio Panebianco <ignaciopanebianco@gmail.com>
2017-10-30 17:16:07 +0000

    adds Balance limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,13 @@
 # Something
 class Oystercard
   attr_accessor :balance
-
+  LIMIT = 90
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    raise 'Balnce limit is £90, try again' if @balance + amount > LIMIT
     @balance += amount
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,7 +3,7 @@ require 'oystercard'
 describe Oystercard do
   let(:card) { described_class.new }
   describe '#balance' do
-    it 'should be initialized with a default value of 0' do
+    it 'should be initialized with a default value of £0' do
       expect(card.balance).to eq(0)
     end
   end
@@ -13,11 +13,14 @@ describe Oystercard do
       card.top_up(5)
       expect(card.balance).to eq(5)
     end
-
     it 'should add the designated sum to the balance' do
       card.top_up(5)
       card.top_up(5)
       expect(card.balance).to eq(10)
+    end
+    it 'should never amount to more than £90' do
+      card.top_up(40)
+      expect { card.top_up(51) }.to raise_error 'Balnce limit is £90, try again'
     end
   end
 end
